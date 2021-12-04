e0c4e4f6ad740cf9cc49c7729f3bb828ac0c7b48
Пётр Макагон <haggard95@yandex.ru>
2021-12-04 22:37:59 +0300

    Initial commit

---
+++ src/main/java/pro/makagon/StoreSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package pro.makagon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreSpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreSpringProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/makagon/StoreSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.makagon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreSpringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
